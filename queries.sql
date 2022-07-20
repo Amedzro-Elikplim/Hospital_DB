ba4685f90731f47de9a8b26f08640c94c6ab61c5
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-07-21 01:26:01 +0300

    add the indexes

--- queries.sql
+++ queries.sql
@@ -1,1 +1,8 @@
-/**/
+--Index medical history foreign key
+CREATE INDEX medical_history_asc ON invoices(medical_history_id ASC);
+
+--Index invoice_id foreign key
+CREATE INDEX invoice_id_asc ON invoice_items(invoice_id ASC);
+
+--Index treatment_id forein key
+CREATE INDEX treatment_id_asc ON invoice_items(treatment_id ASC);
