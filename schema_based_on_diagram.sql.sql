068229ec5785cd3e96bd9af790e0deb35075dc90
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-20 22:08:01 +0000

    Create tables

--- schema_based_on_diagram.sql.sql
+++ schema_based_on_diagram.sql.sql
@@ -1,1 +1,46 @@
-/**All db schema*/
+CREATE TABLE patients (
+    id INT PRIMARY KEY,
+    name VARCHAR(250) NOT NULL,
+    date_of_birth DATE
+);
+
+CREATE TABLE history_treatment_id (
+  id INT PRIMARY KEY
+);
+
+CREATE TABLE medical_histories (
+    id INT PRIMARY KEY,
+    FOREIGN KEY (id) REFERENCES history_treatment_id(id),
+    admitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    patient_id INT,
+    FOREIGN KEY (patient_id) REFERENCES patients(id)
+);
+
+CREATE TABLE invoices (
+    id INT PRIMARY KEY,
+    generated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    medical_history_id INT,
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
+);
+
+CREATE TABLE treatment (
+    id INT PRIMARY KEY,
+    FOREIGN KEY (id) REFERENCES history_treatment_id(id),
+    type VARCHAR(250),
+    name VARCHAR(250)
+);
+
+CREATE TABLE invoice_items (
+    id INT PRIMARY KEY,
+    unit_price DECIMAL(6,2),
+    quantity INT NOT NULL,
+    total_price DECIMAL(6,2),
+    invoice_id INT,
+    FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+    treatment_id INT,
+    FOREIGN KEY (treatment_id) REFERENCES treatment(id)
+);
+
+
+
