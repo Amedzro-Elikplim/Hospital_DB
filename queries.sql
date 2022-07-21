7bf3122bbf2e6225bc83b442fb845f0dbb2b532b
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-21 20:56:40 +0000

    Create index for foreign keys

--- queries.sql
+++ queries.sql
@@ -4,5 +4,12 @@ CREATE INDEX medical_history_asc ON invoices(medical_history_id ASC);
 --Index invoice_id foreign key
 CREATE INDEX invoice_id_asc ON invoice_items(invoice_id ASC);
 
---Index treatment_id forein key
+--Index treatment_id foreign key
 CREATE INDEX treatment_id_asc ON invoice_items(treatment_id ASC);
+
+--Index treatment_fk 
+CREATE INDEX treatment_fk_asc ON history_treatment_id(treatment_fk ASC);
+
+--Index medical_history_fk
+CREATE INDEX medical_history_fk_asc ON history_treatment_id(medical_history_fk ASC);
+
